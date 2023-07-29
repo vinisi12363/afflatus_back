01fe6ed6cc7c540bbf799a2a671c190a12688bc7
vinisi12363 <viniciuspv.si@gmail.com>
2023-07-28 21:14:15 -0300

    fix: fixing the query

--- src/repositories/customers-repository.ts
+++ src/repositories/customers-repository.ts
@@ -3,19 +3,23 @@ import { Customer } from "../Utils/Models/models";
 import { Child } from "../Utils/Models/models";
 //TODO fazer o get customers  com join depois ... 
 
-const getCustomersAndChildById = async(customer_id:number) =>{
+const getCustomersAndChildById = async (customer_id: number) => {
 
-    return await db.query(`select customers.name , customers.birthday , child.name , child.birthday from "customers" INNER JOIN "child" ON child.customer_id =  $1`, [customer_id])
+    return await db.query(` SELECT customers.name AS customer_name, customers.birthday AS customer_birthday,
+    child.name AS child_name, child.birthday AS child_birthday
+    FROM customers
+LEFT JOIN child ON child.customer_id = customers.id
+WHERE customers.id = $1;`, [customer_id])
 
 }
 
-const getCustomers = async ()=> {
+const getCustomers = async () => {
 
     return await db.query(`SELECT * FROM "customers"`)
 
- }
+}
 
- const insertCustomer = async (customer: Customer)=> {
+const insertCustomer = async (customer: Customer) => {
 
     const customerInsert = await db.query(`insert into "customers" (name, email, address, phone, instagram , birthday) 
     values ($1 , $2,$3 , $4 , $5 , $5) RETURNING id`, [customer.name, customer.email, customer.address, customer.phone, customer.instagram, customer.birthday])
