f5df443aceb1e2e7ecf41c3d2996c2592ebaeb85
vinisi12363 <viniciuspv.si@gmail.com>
2023-10-18 23:18:07 -0300

    feat: finaling the events route

--- src/controller/events-controller.ts
+++ src/controller/events-controller.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
+import { eventsService } from "@/services";
 
 
 export const getEvents = async (req: Request, res: Response) => {
     try {
         //get events here
-
-        return res.status(httpStatus.OK).send("events");
+        const result = await eventsService.getEvents();
+        console.log("RESULT", result);
+        return res.status(httpStatus.OK).send(result.rows);
     } catch (error) {
         return res.status(httpStatus.BAD_REQUEST).send(error);
     }
--- src/repositories/events-repository.ts
+++ src/repositories/events-repository.ts
@@ -1,6 +1,19 @@
 import { db } from "@/config";
 
-export const getEvents = async () => {};
+export const getEvents = async () => {
+    return await db.query(`SELECT 		
+            cus.name,
+            cus.birthday,
+            chil.name AS child_name,
+            chil.birthday AS child_birthday,
+            spd.description AS description,
+            spd.special_date AS special_date
+        FROM "customers" AS cus 
+        INNER JOIN "child" AS chil ON cus.id = chil.customer_id
+        LEFT JOIN "SpecialDate" AS spd ON chil.id = spd.child_id;
+    `);
+
+};
 
 
 export const eventsRepository = {
