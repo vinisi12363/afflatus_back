78c4c51a622707acbee42d985b4a21cda8f89de0
vinisi12363 <viniciuspv.si@gmail.com>
2023-10-26 09:04:48 -0300

    fix: fixintg the code to deploy version

--- src/config/connectdb.ts
+++ src/config/connectdb.ts
@@ -1,12 +1,20 @@
-import { Pool } from "pg";
+import pg from "pg";
 import dotenv from "dotenv";
-
 dotenv.config();
 
+const { Pool } = pg;
+
 const configDatabase = {
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+};
+
+if (process.env.MODE === "prod") {
+    configDatabase.ssl = {
+        rejectUnauthorized: true
+    };
 }
- new Pool({
-    
- })
-export const db = new Pool(configDatabase)
+
+export const db = new Pool(configDatabase);
